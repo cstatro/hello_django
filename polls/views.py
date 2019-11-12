66d86c4b969624613462e4371a6ce6a23c2a68a0
Colin <cstatro@gmail.com>
2019-11-11 22:31:52 -0500

    committing custom route

--- polls/views.py
+++ polls/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import HttpResponse
 
 def index(request):
     return HttpResponse('Welcome to the polls buddy!')
+
+
+def details(request, question_id):
+    return HttpResponse(f"You're looking at {question_id}.")
